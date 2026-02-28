0cc66a3d81be782ce3fe3fe7abf190fd2d4e4ce3
Shrujan Sriram <shrujan.sriram@gmail.com>
2026-02-27 23:35:11 -0800

    Instructions

--- python/parse_recipe.py
+++ python/parse_recipe.py
@@ -1,5 +1,12 @@
-def parse_recipe():
-    ...
-
+def parse_recipe(data):
+    item_name = data["data"]["name"]
+    instructions = []
+    for i in data["data"]["instructions"]:
+        text = i["text"]
+        duration = i["structured"]["duration"] if i["structured"]["duration"] else None
+        action = i["structured"]["action"] if i["structured"]["action"] else None
+        temperature = i["structured"]["temperature"]["fahrenheit"] if i["structured"]["temperature"]["fahrenheit"] else None
+        instructions.append((text, action, duration,  temperature))
+    #return instructions
 def get_recipe():
     pass
